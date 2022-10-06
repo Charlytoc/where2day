e6b858e282186be6a112a505644efab43d417713
Wishmastering <charsdb@gmail.com>
2022-10-06 02:10:41 +0000

    Navbar Estetico Listo / NO CONECTADO CON EL BACK DE NINGUNA MANERA

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -2,18 +2,23 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 export const Navbar = () => {
-	return (
-		<nav className="navbar navbar-light bg-light">
-			<div className="container">
-				<Link to="/">
-					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-				</Link>
-				<div className="ml-auto">
-					<Link to="/demo">
-						<button className="btn btn-primary">Check the Context in action</button>
-					</Link>
-				</div>
-			</div>
-		</nav>
-	);
+  return (
+    <nav className="navbar ">
+      <div className="container">
+        <Link to="/">
+          <span className="navbar-brand h1">Where2DayLOGO</span>
+        </Link>
+        <div className="">
+          <Link to="/login">
+            {/* ESTA RUTA ME DEBE DE LLEVAR A LOGIN*/}
+            <button className="btn btn-primary">Login</button>
+          </Link>
+          <Link to="/signup">
+            {/* ESTA RUTA ME DEBE DE LLEVAR A SIGNUP DADO EL CASO */}
+            <button className="btn btn-primary ms-3">Signup</button>
+          </Link>
+        </div>
+      </div>
+    </nav>
+  );
 };
