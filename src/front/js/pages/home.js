acc6f22120a8c91d4b2d35ca7fbb0889b2b936ff
Franz Seidel <franz8818@gmail.com>
2022-10-06 03:13:19 +0000

    avance de landing page

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -1,43 +1,155 @@
-import React, {
-    useContext
-} from "react";
-import {
-    Context
-} from "../store/appContext";
-import rigoImageUrl from "../../img/rigo-baby.jpg";
+import React, { useContext } from "react";
+import { Context } from "../store/appContext";
 import "../../styles/home.css";
 
 export const Home = () => {
-    const {
-        store,
-        actions
-    } = useContext(Context);
+  //   const { store, actions } = useContext(Context);
 
-    return ( <
-        div className = "text-center mt-5" >
-        <
-        h1 > Hello Rigo!! < /h1> <
-        h1 > where2do < /h1> <
-        p >
-        <
-        img src = {
-            rigoImageUrl
-        }
-        /> <
-        /p> <
-        div className = "alert alert-info" > {
-            store.message ||
-            "Loading message from the backend (make sure your python backend is running)..."
-        } <
-        /div> <
-        p >
-        This boilerplate comes with lots of documentation: {
-            " "
-        } <
-        a href = "https://github.com/4GeeksAcademy/react-flask-hello/tree/95e0540bd1422249c3004f149825285118594325/docs" >
-        Read documentation <
-        /a> <
-        /p> <
-        /div>
-    );
+  return (
+    <>
+      {/* <div
+        id="carouselExampleDark"
+        className="carousel carousel-dark slide"
+        data-bs-ride="carousel"
+      >
+        <div className="carousel-indicators">
+          <button
+            type="button"
+            data-bs-target="#carouselExampleDark"
+            data-bs-slide-to="0"
+            className="active"
+            aria-current="true"
+            aria-label="Slide 1"
+          ></button>
+          <button
+            type="button"
+            data-bs-target="#carouselExampleDark"
+            data-bs-slide-to="1"
+            aria-label="Slide 2"
+          ></button>
+          <button
+            type="button"
+            data-bs-target="#carouselExampleDark"
+            data-bs-slide-to="2"
+            aria-label="Slide 3"
+          ></button>
+        </div>
+        <div className="carousel-inner">
+          <div className="carousel-item active" data-bs-interval="10000">
+            <img
+              src="http://www.superpark.com.sg/wp-content/uploads/2016/11/SG_Skate-145.jpg"
+              className="d-block w-100"
+              alt="..."
+            />
+            <div className="carousel-caption d-none d-md-block"></div>
+          </div>
+          <div className="carousel-item" data-bs-interval="2000">
+            <img
+              src="https://www.simplemost.com/wp-content/uploads/2015/07/dinner-party.jpg"
+              className="d-block w-100"
+              alt="..."
+            />
+            <div className="carousel-caption d-none d-md-block"></div>
+          </div>
+          <div className="carousel-item">
+            <img
+              src="https://www.muscleandfitness.com/wp-content/uploads/2017/08/Female-kayaking-in-cavernous-river.jpg?quality=86&strip=all"
+              className="d-block w-100"
+              alt="..."
+            />
+            <div className="carousel-caption d-none d-md-block"></div>
+          </div>
+        </div>
+        <button
+          className="carousel-control-prev"
+          type="button"
+          data-bs-target="#carouselExampleDark"
+          data-bs-slide="prev"
+        >
+          <span
+            className="carousel-control-prev-icon"
+            aria-hidden="true"
+          ></span>
+          <span className="visually-hidden">Previous</span>
+        </button>
+        <button
+          className="carousel-control-next"
+          type="button"
+          data-bs-target="#carouselExampleDark"
+          data-bs-slide="next"
+        >
+          <span
+            className="carousel-control-next-icon"
+            aria-hidden="true"
+          ></span>
+          <span className="visually-hidden">Next</span>
+        </button>
+      </div> */}
+
+      <div
+        id="carouselExampleInterval"
+        className="carousel slide"
+        data-bs-ride="carousel"
+      >
+        <div className="carousel-inner">
+          <div className="carousel-item active" data-bs-interval="10000">
+            <img
+              src="http://www.superpark.com.sg/wp-content/uploads/2016/11/SG_Skate-145.jpg"
+              className="d-block w-100"
+              alt="..."
+            />
+          </div>
+          <div className="carousel-item" data-bs-interval="2000">
+            <img
+              src="https://www.simplemost.com/wp-content/uploads/2015/07/dinner-party.jpg"
+              className="d-block w-100"
+              alt="..."
+            />
+          </div>
+          <div className="carousel-item">
+            <img
+              src="https://www.muscleandfitness.com/wp-content/uploads/2017/08/Female-kayaking-in-cavernous-river.jpg?quality=86&strip=all"
+              className="d-block w-100"
+              alt="..."
+            />
+          </div>
+        </div>
+        <button
+          className="carousel-control-prev"
+          type="button"
+          data-bs-target="#carouselExampleInterval"
+          data-bs-slide="prev"
+        >
+          <span
+            className="carousel-control-prev-icon"
+            aria-hidden="true"
+          ></span>
+          <span className="visually-hidden">Previous</span>
+        </button>
+        <button
+          className="carousel-control-next"
+          type="button"
+          data-bs-target="#carouselExampleInterval"
+          data-bs-slide="next"
+        >
+          <span
+            className="carousel-control-next-icon"
+            aria-hidden="true"
+          ></span>
+          <span className="visually-hidden">Next</span>
+        </button>
+      </div>
+
+      <p>
+        {" "}
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam eum
+        nostrum fuga, minus veritatis sed dignissimos, cupiditate asperiores
+        ipsum quaerat inventore eaque, nesciunt nobis laudantium laborum itaque
+        repellendus ipsa quia.{" "}
+      </p>
+      <h2>FACEBOOK</h2>
+      <h2>INSTAGRAM</h2>
+      <h2>TWITTER</h2>
+    </>
+  );
 };
