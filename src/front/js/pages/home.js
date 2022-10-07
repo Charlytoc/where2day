aeb028ccf715009b98101680fb5c2a88c894093a
Franz Seidel <franz8818@gmail.com>
2022-10-07 02:23:13 +0000

    AVANCE LANDING

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -1,145 +1,71 @@
 import React, { useContext } from "react";
 import { Context } from "../store/appContext";
 import "../../styles/home.css";
+import rigoImageUrl from "../../img/Where2Day.png";
 
 export const Home = () => {
   //   const { store, actions } = useContext(Context);
 
   return (
-    <>
-      {/* <div
-        id="carouselExampleDark"
-        className="carousel carousel-dark slide"
-        data-bs-ride="carousel"
-      >
-        <div className="carousel-indicators">
-          <button
-            type="button"
-            data-bs-target="#carouselExampleDark"
-            data-bs-slide-to="0"
-            className="active"
-            aria-current="true"
-            aria-label="Slide 1"
-          ></button>
-          <button
-            type="button"
-            data-bs-target="#carouselExampleDark"
-            data-bs-slide-to="1"
-            aria-label="Slide 2"
-          ></button>
-          <button
-            type="button"
-            data-bs-target="#carouselExampleDark"
-            data-bs-slide-to="2"
-            aria-label="Slide 3"
-          ></button>
-        </div>
-        <div className="carousel-inner">
-          <div className="carousel-item active" data-bs-interval="10000">
-            <img
-              src="http://www.superpark.com.sg/wp-content/uploads/2016/11/SG_Skate-145.jpg"
-              className="d-block w-100"
-              alt="..."
-            />
-            <div className="carousel-caption d-none d-md-block"></div>
-          </div>
-          <div className="carousel-item" data-bs-interval="2000">
-            <img
-              src="https://www.simplemost.com/wp-content/uploads/2015/07/dinner-party.jpg"
-              className="d-block w-100"
-              alt="..."
-            />
-            <div className="carousel-caption d-none d-md-block"></div>
-          </div>
-          <div className="carousel-item">
-            <img
-              src="https://www.muscleandfitness.com/wp-content/uploads/2017/08/Female-kayaking-in-cavernous-river.jpg?quality=86&strip=all"
-              className="d-block w-100"
-              alt="..."
-            />
-            <div className="carousel-caption d-none d-md-block"></div>
-          </div>
-        </div>
-        <button
-          className="carousel-control-prev"
-          type="button"
-          data-bs-target="#carouselExampleDark"
-          data-bs-slide="prev"
-        >
-          <span
-            className="carousel-control-prev-icon"
-            aria-hidden="true"
-          ></span>
-          <span className="visually-hidden">Previous</span>
-        </button>
-        <button
-          className="carousel-control-next"
-          type="button"
-          data-bs-target="#carouselExampleDark"
-          data-bs-slide="next"
-        >
-          <span
-            className="carousel-control-next-icon"
-            aria-hidden="true"
-          ></span>
-          <span className="visually-hidden">Next</span>
-        </button>
-      </div> */}
-
-      <div
-        id="carouselExampleInterval"
-        className="carousel slide"
-        data-bs-ride="carousel"
-      >
-        <div className="carousel-inner">
-          <div className="carousel-item active" data-bs-interval="10000">
-            <img
-              src="http://www.superpark.com.sg/wp-content/uploads/2016/11/SG_Skate-145.jpg"
-              className="d-block w-100"
-              alt="..."
-            />
-          </div>
-          <div className="carousel-item" data-bs-interval="2000">
-            <img
-              src="https://www.simplemost.com/wp-content/uploads/2015/07/dinner-party.jpg"
-              className="d-block w-100"
-              alt="..."
-            />
-          </div>
-          <div className="carousel-item">
-            <img
-              src="https://www.muscleandfitness.com/wp-content/uploads/2017/08/Female-kayaking-in-cavernous-river.jpg?quality=86&strip=all"
-              className="d-block w-100"
-              alt="..."
-            />
-          </div>
-        </div>
-        <button
-          className="carousel-control-prev"
-          type="button"
-          data-bs-target="#carouselExampleInterval"
-          data-bs-slide="prev"
-        >
-          <span
-            className="carousel-control-prev-icon"
-            aria-hidden="true"
-          ></span>
-          <span className="visually-hidden">Previous</span>
-        </button>
-        <button
-          className="carousel-control-next"
-          type="button"
-          data-bs-target="#carouselExampleInterval"
-          data-bs-slide="next"
-        >
-          <span
-            className="carousel-control-next-icon"
-            aria-hidden="true"
-          ></span>
-          <span className="visually-hidden">Next</span>
-        </button>
+    <div className="row mt-5">
+      <div className="col-10 offset-1 col-md-8 offset-md-2">
+        <img src={rigoImageUrl} className="img-fluid" /> <hr className="my-4" />
       </div>
-
+      <div className="col-12 col-md-8 offset-md-2">
+        <div
+          id="carouselExampleInterval"
+          className="carousel slide"
+          data-bs-ride="carousel"
+        >
+          <div className="carousel-inner">
+            <div className="carousel-item active" data-bs-interval="10000">
+              <img
+                src="http://www.superpark.com.sg/wp-content/uploads/2016/11/SG_Skate-145.jpg"
+                className="d-block w-100"
+                alt="..."
+              />
+            </div>
+            <div className="carousel-item" data-bs-interval="2000">
+              <img
+                src="https://www.simplemost.com/wp-content/uploads/2015/07/dinner-party.jpg"
+                className="d-block w-100"
+                alt="..."
+              />
+            </div>
+            <div className="carousel-item">
+              <img
+                src="https://www.muscleandfitness.com/wp-content/uploads/2017/08/Female-kayaking-in-cavernous-river.jpg?quality=86&strip=all"
+                className="d-block w-100"
+                alt="..."
+              />
+            </div>
+          </div>
+          <button
+            className="carousel-control-prev"
+            type="button"
+            data-bs-target="#carouselExampleInterval"
+            data-bs-slide="prev"
+          >
+            <span
+              className="carousel-control-prev-icon"
+              aria-hidden="true"
+            ></span>
+            <span className="visually-hidden">Previous</span>
+          </button>
+          <button
+            className="carousel-control-next"
+            type="button"
+            data-bs-target="#carouselExampleInterval"
+            data-bs-slide="next"
+          >
+            <span
+              className="carousel-control-next-icon"
+              aria-hidden="true"
+            ></span>
+            <span className="visually-hidden">Next</span>
+          </button>
+        </div>
+      </div>
       <p>
         {" "}
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam eum
@@ -147,9 +73,11 @@ export const Home = () => {
         ipsum quaerat inventore eaque, nesciunt nobis laudantium laborum itaque
         repellendus ipsa quia.{" "}
       </p>
-      <h2>FACEBOOK</h2>
+      <h2>
+        FACEBOOK <i class="bi bi-facebook"></i>
+      </h2>
       <h2>INSTAGRAM</h2>
       <h2>TWITTER</h2>
-    </>
+    </div>
   );
 };
