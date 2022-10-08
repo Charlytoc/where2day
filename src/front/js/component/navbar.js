ca9418f93f0d1eed122addbe9686d2b9d7ae02e1
Charlytoc <charlyjchaconc@gmail.com>
2022-10-08 05:49:19 +0000

    landing iniciado

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -1,16 +1,17 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import  logoPres from "../../img/logo-presentation.png"
 
 export const Navbar = () => {
 	return (
 		<nav className="navbar navbar-light bg-light">
 			<div className="container">
 				<Link to="/">
-					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
+      <img src={logoPres} alt="Logo" width="100" height="70" class="d-block align-text-top" />
 				</Link>
 				<div className="ml-auto">
 					<Link to="/demo">
-						<button className="btn btn-primary">Check the Context in action</button>
+						<button className="btn btn-warning">Me quiero ir un rato</button>
 					</Link>
 				</div>
 			</div>
--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -1,26 +1,30 @@
 import React, { useContext } from "react";
 import { Context } from "../store/appContext";
-import rigoImageUrl from "../../img/rigo-baby.jpg";
+import logo from "../../img/logo.png";
 import "../../styles/home.css";
 
 export const Home = () => {
 	const { store, actions } = useContext(Context);
 
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!!</h1>
-			<p>
-				<img src={rigoImageUrl} />
-			</p>
-			<div className="alert alert-info">
-				{store.message || "Loading message from the backend (make sure your python backend is running)..."}
-			</div>
-			<p>
-				This boilerplate comes with lots of documentation:{" "}
-				<a href="https://github.com/4GeeksAcademy/react-flask-hello/tree/95e0540bd1422249c3004f149825285118594325/docs">
-					Read documentation
-				</a>
-			</p>
+		<div className="text-center w-50 container mt-5">
+			<img src={logo} style={{width: 300}} />
+			<form>
+  <div class="mb-3">
+    <input type="email" placeholder="Introduce aquí tu email, buenaventurado." class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
+  </div>
+  <div class="mb-3">
+    <input type="password" placeholder="Y por acá tu clave secreta" class="form-control" id="exampleInputPassword1"/>
+  </div>
+  <div class="mb-3 form-check">
+    
+    <label class="form-check-label" for="exampleCheck1">¿Quieres vivir las mejores experiencas de tu vida?</label>
+	<input type="checkbox" class="" id="exampleCheck1"/>
+  </div>
+  
+  <button type="submit" class="btn btn-warning">Unirme a la diversión</button>
+</form>
 		</div>
+
 	);
 };
