cdc43b52d4c4deaf5f02ff320462cbd02d8ac184
Wishmastering <charsdb@gmail.com>
2022-10-11 04:39:34 +0000

    Navbar DINAMICO Login/Logout dependiendo de estado de AUTH

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -6,11 +6,9 @@ import logoPres from "../../img/logo-presentation.png";
 import { useContext } from "react"; // #1 Traer context de react
 import { Context } from "../store/appContext"; // #2 traer nuestro context
 
-
 export const Navbar = () => {
+  const { store, actions } = useContext(Context); // #3 Consumirlo
 
-  const { actions } = useContext(Context); // #3 Consumirlo
-  
   return (
     <nav className="navbar bg-light" style={{ height: 75 }}>
       <div className="container">
@@ -19,19 +17,34 @@ export const Navbar = () => {
             <img src={logoPres} style={{ height: 60 }} />
           </span>
         </Link>
-        <div className="">
-          <Link to="/login">
-            {/* ESTA RUTA ME DEBE DE LLEVAR A LOGIN*/}
-            <button className="btn btn-warning">Login</button>
-          </Link>
-          <Link to="/signup">
-            {/* ESTA RUTA ME DEBE DE LLEVAR A SIGNUP DADO EL CASO */}
-            <button className="btn btn-warning ms-3">Signup</button>
-          </Link>
-        
-          <button className="btn btn-warning ms-3" onClick={()=> actions.logout()}>Logout</button>
+        <div>
+          {!store.auth ? (
+            <>
+              <Link to="/login">
+                <button className="btn btn-warning">Login</button>
+              </Link>
+
+              <Link to="/signup">
+                <button className="btn btn-warning ms-3">Signup</button>
+              </Link>
+            </>
+          ) : (
+            <button
+              className="btn btn-warning ms-3"
+              onClick={() => actions.logout()}
+            >
+              Logout
+            </button>
+          )}
         </div>
       </div>
     </nav>
   );
 };
+{
+  /* ESTA RUTA ME DEBE DE LLEVAR A LOGIN*/
+}
+
+{
+  /* ESTA RUTA ME DEBE DE LLEVAR A SIGNUP DADO EL CASO */
+}
