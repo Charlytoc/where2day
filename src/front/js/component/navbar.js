9691327d55bf7b960375f13d2376c648596278f7
Franz Seidel <franz8818@gmail.com>
2022-10-14 22:48:21 +0000

    CAMBIO DE COLORES Y LOGO EN NAVBAR

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -1,50 +1,43 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import {
+    Link
+} from "react-router-dom";
 import logo from "../../img/logo.png";
-import logoPres from "../../img/logo-presentation.png";
-
-import { useContext } from "react"; // #1 Traer context de react
-import { Context } from "../store/appContext"; // #2 traer nuestro context
+import logoPres from "../../img/Logo WHERE2DAY.png";
 
 export const Navbar = () => {
-  const { store, actions } = useContext(Context); // #3 Consumirlo
-
-  return (
-    <nav className="navbar bg-light" style={{ height: 75 }}>
-      <div className="container">
-        <Link to="/">
-          <span className="navbar-brand h1">
-            <img src={logoPres} style={{ height: 60 }} />
-          </span>
-        </Link>
-        <div>
-          {!store.auth ? (
-            <>
-              <Link to="/login">
-                <button className="btn btn-warning">Login</button>
-              </Link>
-
-              <Link to="/signup">
-                <button className="btn btn-warning ms-3">Signup</button>
-              </Link>
-            </>
-          ) : (
-            <button
-              className="btn btn-warning ms-3"
-              onClick={() => actions.logout()}
-            >
-              Logout
-            </button>
-          )}
-        </div>
-      </div>
-    </nav>
-  );
+    return ( <
+        nav className = "navbar" >
+        <
+        div className = "container" >
+        <
+        Link to = "/" >
+        <
+        span className = "navbar-brand h1" >
+        <
+        img src = {
+            logoPres
+        }
+        style = {
+            {
+                height: 60,
+            }
+        }
+        /> <
+        /span> <
+        /Link> <
+        div className = "" >
+        <
+        Link to = "/login" > {
+            /* ESTA RUTA ME DEBE DE LLEVAR A LOGIN*/ } <
+        button className = "btn btn-sm bg-redw2d" > Login < /button> <
+        /Link> <
+        Link to = "/signup" > {
+            /* ESTA RUTA ME DEBE DE LLEVAR A SIGNUP DADO EL CASO */ } <
+        button className = "btn btn-sm bg-redw2d ms-2" > Signup < /button> <
+        /Link> <
+        /div> <
+        /div> <
+        /nav>
+    );
 };
-{
-  /* ESTA RUTA ME DEBE DE LLEVAR A LOGIN*/
-}
-
-{
-  /* ESTA RUTA ME DEBE DE LLEVAR A SIGNUP DADO EL CASO */
-}
