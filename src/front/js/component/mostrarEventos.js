476daf4383af2041512dbae2d50be0896a7d135b
Franz Seidel <franz8818@gmail.com>
2022-10-18 01:20:27 +0000

    avance de eventos en feed

--- src/front/js/component/mostrarEventos.js
+++ src/front/js/component/mostrarEventos.js
@@ -7,40 +7,38 @@ import { useContext } from "react"; // #1 Traer context de react
 import { Context } from "../store/appContext"; // #2 traer nuestro context
 
 export const MostrarEventos = () => {
-
   const { store, actions } = useContext(Context); // #3 Consumirlo
 
+  //ACA LLAMAMOS LA FUNCION QUE HACE EL FETCH QUE VA TRAER LAS EXPERIENCIAS QUE TENEMOS DE DB
+
   return (
-   <>
-     <div className="card text-bg-light mb-3">
-              {/* CARD EVENTOS */}
-              <h5>
-                <div className="card-header font-weight-bold bg-yellw2d">
-                  Eventos
-                </div>
-              </h5>
-              <div className="card-body">
-                <h5 className="card-title"> Festival montañitas </h5>
-                <img
-                  src="https://media.traveler.es/photos/61376a65ba2a75fcba4be8d5/master/w_1600%2Cc_limit/148355.jpg"
-                  className="d-block w-100"
-                  alt="..."
-                />
-                <p>
-                  <b>Lugar:</b> Guayaquil, Ecuador
-                </p>
-                <p className="card-text">
-                  Lorem ipsum dolor sit amet consectetur adipisicing
-                  elit.Quibusdam eum nostrum fuga, minus veritatis sed
-                  dignissimos, cupiditate asperiores ipsum quaerat inventore
-                  eaque, nesciunt nobis laudantium laborum itaque repellendus
-                  ipsa quia2.
-                </p>
-              </div>
-            </div>
-   </>
-
-
-        
-  )
-}
+    <>
+      <div className="card text-bg-light mb-3">
+        {" "}
+        {/* CARD EVENTOS */}{" "}
+        <h5>
+          <div className="card-header font-weight-bold bg-yellw2d">
+            Eventos{" "}
+          </div>{" "}
+        </h5>{" "}
+        <div className="card-body">
+          <h5 className="card-title"> Festival montañitas </h5>{" "}
+          <img
+            src="https://media.traveler.es/photos/61376a65ba2a75fcba4be8d5/master/w_1600%2Cc_limit/148355.jpg"
+            className="d-block w-100"
+            alt="..."
+          />
+          <p>
+            <b> Lugar: </b> Guayaquil, Ecuador{" "}
+          </p>{" "}
+          <p className="card-text">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.Quibusdam
+            eum nostrum fuga, minus veritatis sed dignissimos, cupiditate
+            asperiores ipsum quaerat inventore eaque, nesciunt nobis laudantium
+            laborum itaque repellendus ipsa quia2.{" "}
+          </p>{" "}
+        </div>{" "}
+      </div>{" "}
+    </>
+  );
+};
--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -1,185 +1,225 @@
-const getState = ({ getStore, getActions, setStore }) => {
-  return {
-    store: {
-      // message: null,
-      // // demo: [
-      // //   {
-      // //     title: "FIRST",
-      // //     background: "white",
-      // //     initial: "white",
-      // //   },
-      // //   {
-      // //     title: "SECOND",
-      // //     background: "white",
-      // //     initial: "white",
-      // //   },
-      // // ],
+const getState = ({
+    getStore,
+    getActions,
+    setStore
+}) => {
+    return {
+        store: {
+            // message: null,
+            // // demo: [
+            // //   {
+            // //     title: "FIRST",
+            // //     background: "white",
+            // //     initial: "white",
+            // //   },
+            // //   {
+            // //     title: "SECOND",
+            // //     background: "white",
+            // //     initial: "white",
+            // //   },
+            // // ],
 
-      auth: false,
-      usuario_actual: 0,
-      redirectLogin: false
-    },
-    actions: {
-      // Use getActions to call a function within a fuction
-      exampleFunction: () => {
-        getActions().changeColor(0, "green");
-      },
+            auth: false,
+            usuario_actual: 0,
+            redirectLogin: false,
 
-      getMessage: async () => {
-        try {
-          // fetching data from the backend
-          const resp = await fetch(process.env.BACKEND_URL + "/api/hello");
-          const data = await resp.json();
-          setStore({
-            message: data.message,
-          });
-          // don't forget to return something, that is how the async resolves
-          return data;
-        } catch (error) {
-          console.log("Error loading message from backend", error);
-        }
-      },
+            mostrarEventos: [],
 
+        },
+        actions: {
+            // Use getActions to call a function within a fuction
+            exampleFunction: () => {
+                getActions().changeColor(0, "green");
+            },
 
-      // EL ASYNC SIEMPRE DEBE IR ACOMPA:ADO DE UN AWAIT, es como un "if/else", son dependientes
-      login: async (email, password) => {
-        // el try INTENTARA hacer lo que se encuentra entre "{}", SINO funciona, omite la logica que ahi se encuentra
-        try{
-          const response = await fetch(process.env.BACKEND_URL + "/api/login", {
-          method: "POST",
-          body: JSON.stringify({
-            email: email,
-            password: password,
-                              }),
-          headers: {
-            "Content-Type": "application/json",
-                    },
-                                                                              });
-        // Una vez que funciono el "try" y los datos se trajeron, entonces 
-        // Hacemos un if/else con el status recibido
-        
-        // Si es un 401, entonces auth:false
-        if (!response.status === 200){
-          // console.log(response)
-          alert('Wrong eMail or Password, please try again');
-          setStore({auth:false});
-                                      }else{
-        const data = await response.json()
-        // console.log(data.access_token)
-        localStorage.setItem("token", data.access_token)
-        setStore({ auth: true });
-        // const store = getStore();
-        // console.log(store.auth)
-        return true;    
-                                             }
-        // Caso contrario SI PONEMOS EL auth:true
-             } 
-      // Ahora... Si el TRY NO SIRVIO, entonces INMEDIATAMENTE HAREMOS UN CATCH
-      // El catch NOS DIRA donde viene el error o porque el async/await no sirvio
-      catch(error){
-        if(error.msg === 'Bad email and password'){
-          alert('Wrong eMail or Password, please try again');
-                                                   }
-        setStore({auth:false});
-          console.log(error);
-                   }
-      },
-
-
-
-     
-      // EL ASYNC SIEMPRE DEBE IR ACOMPA:ADO DE UN AWAIT, es como un "if/else", son dependientes
-      signup: async (email, password) => {
-         // el try INTENTARA hacer lo que se encuentra entre "{}", SINO funciona, omite la logica que ahi se encuentra
-        try{
-          const response = await fetch(process.env.BACKEND_URL + "/api/signup", {
-          method: "POST",
-          body: JSON.stringify({
-            email: email,
-            password: password,
-                               }),
-          headers: {
-            "Content-Type": "application/json",
-                   },
-                                                                                });
-        // Una vez que funciono el "try" y los datos se trajeron, entonces 
-        // Hacemos un if/else con el status recibido
-        
-        if(response.status === 200){
-          const data = await response.json()
-        setStore({redirectLogin: true});
-        (alert('An email has been sent, please follow instructions'))
-      }
-        else  {(alert("Este correo ya se ha registrado"))}
-           
-            
-           }
-      // Ahora... Si el TRY NO SIRVIO, entonces INMEDIATAMENTE HAREMOS UN CATCH
-      // El catch NOS DIRA donde viene el error o porque el async/await no sirvio
-      catch (error) {
-        console.log("Error loading message from backend", error);
-                     }   
-      },
-
-      logout: () => {
-        setStore({ auth: false });
-        const store = getStore();
-        console.log(store.auth);
-      },
-
-      obtenerUsuario: () => {
-                console.log("funciono desde el flux")
-				let token = localStorage.getItem("token")
-				
-          fetch(process.env.BACKEND_URL + "/api/getuser", {
-             method: "GET",
-              headers: {
-              'Content-Type': 'application/json',
-							'Authorization': "Bearer "+token} })
-         .then((response) => response.json())
-          .then((data) => setStore({usuario_actual: data}))},
-
-      postear: (titulo, lugar, description, fecha, outdoor, indoor, anywhere) => {
-        const store = getStore()
-
-        fetch(process.env.BACKEND_URL + "/api/postear", {
-                method: "POST",
-                body: JSON.stringify({
-                    titulo: titulo,
-                    lugar: lugar,
-                     description: description,
-                    usuario_id: store.usuario_actual,
-                    fecha: fecha,
-                    outdoor: outdoor,
-                    indoor: indoor,
-                    anywhere: anywhere
-
-                }),
-                headers: {
-                    'Content-Type': 'application/json'
+            getMessage: async () => {
+                try {
+                    // fetching data from the backend
+                    const resp = await fetch(process.env.BACKEND_URL + "/api/hello");
+                    const data = await resp.json();
+                    setStore({
+                        message: data.message,
+                    });
+                    // don't forget to return something, that is how the async resolves
+                    return data;
+                } catch (error) {
+                    console.log("Error loading message from backend", error);
                 }
-            })
-            .then((response) => response.json())
-            .then((data) => console.log(data))},
+            },
 
-      changeColor: (index, color) => {
-        //get the store
-        const store = getStore();
 
-        //we have to loop the entire demo array to look for the respective index
-        //and change its color
-        const demo = store.demo.map((elm, i) => {
-          if (i === index) elm.background = color;
-          return elm;
-        });
+            // EL ASYNC SIEMPRE DEBE IR ACOMPA:ADO DE UN AWAIT, es como un "if/else", son dependientes
+            login: async (email, password) => {
+                // el try INTENTARA hacer lo que se encuentra entre "{}", SINO funciona, omite la logica que ahi se encuentra
+                try {
+                    const response = await fetch(process.env.BACKEND_URL + "/api/login", {
+                        method: "POST",
+                        body: JSON.stringify({
+                            email: email,
+                            password: password,
+                        }),
+                        headers: {
+                            "Content-Type": "application/json",
+                        },
+                    });
+                    // Una vez que funciono el "try" y los datos se trajeron, entonces 
+                    // Hacemos un if/else con el status recibido
 
-        //reset the global store
-        setStore({
-          demo: demo,
-        });
-      },
-    },
-  };
+                    // Si es un 401, entonces auth:false
+                    if (!response.status === 200) {
+                        // console.log(response)
+                        alert('Wrong eMail or Password, please try again');
+                        setStore({
+                            auth: false
+                        });
+                    } else {
+                        const data = await response.json()
+                        // console.log(data.access_token)
+                        localStorage.setItem("token", data.access_token)
+                        setStore({
+                            auth: true
+                        });
+                        // const store = getStore();
+                        // console.log(store.auth)
+                        return true;
+                    }
+                    // Caso contrario SI PONEMOS EL auth:true
+                }
+                // Ahora... Si el TRY NO SIRVIO, entonces INMEDIATAMENTE HAREMOS UN CATCH
+                // El catch NOS DIRA donde viene el error o porque el async/await no sirvio
+                catch (error) {
+                    if (error.msg === 'Bad email and password') {
+                        alert('Wrong eMail or Password, please try again');
+                    }
+                    setStore({
+                        auth: false
+                    });
+                    console.log(error);
+                }
+            },
+
+
+
+
+            // EL ASYNC SIEMPRE DEBE IR ACOMPA:ADO DE UN AWAIT, es como un "if/else", son dependientes
+            signup: async (email, password) => {
+                // el try INTENTARA hacer lo que se encuentra entre "{}", SINO funciona, omite la logica que ahi se encuentra
+                try {
+                    const response = await fetch(process.env.BACKEND_URL + "/api/signup", {
+                        method: "POST",
+                        body: JSON.stringify({
+                            email: email,
+                            password: password,
+                        }),
+                        headers: {
+                            "Content-Type": "application/json",
+                        },
+                    });
+                    // Una vez que funciono el "try" y los datos se trajeron, entonces 
+                    // Hacemos un if/else con el status recibido
+
+                    if (response.status === 200) {
+                        const data = await response.json()
+                        setStore({
+                            redirectLogin: true
+                        });
+                        (alert('An email has been sent, please follow instructions'))
+                    } else {
+                        (alert("Este correo ya se ha registrado"))
+                    }
+
+
+                }
+                // Ahora... Si el TRY NO SIRVIO, entonces INMEDIATAMENTE HAREMOS UN CATCH
+                // El catch NOS DIRA donde viene el error o porque el async/await no sirvio
+                catch (error) {
+                    console.log("Error loading message from backend", error);
+                }
+            },
+
+            logout: () => {
+                setStore({
+                    auth: false
+                });
+                const store = getStore();
+                console.log(store.auth);
+            },
+
+            obtenerUsuario: () => {
+                console.log("funciono desde el flux")
+                let token = localStorage.getItem("token")
+
+                fetch(process.env.BACKEND_URL + "/api/getuser", {
+                        method: "GET",
+                        headers: {
+                            'Content-Type': 'application/json',
+                            'Authorization': "Bearer " + token
+                        }
+                    })
+                    .then((response) => response.json())
+                    .then((data) => setStore({
+                        usuario_actual: data
+                    }))
+            },
+
+            postear: (titulo, lugar, description, fecha, outdoor, indoor, anywhere) => {
+                const store = getStore()
+
+                fetch(process.env.BACKEND_URL + "/api/postear", {
+                        method: "POST",
+                        body: JSON.stringify({
+                            titulo: titulo,
+                            lugar: lugar,
+                            description: description,
+                            usuario_id: store.usuario_actual,
+                            fecha: fecha,
+                            outdoor: outdoor,
+                            indoor: indoor,
+                            anywhere: anywhere
+
+                        }),
+                        headers: {
+                            'Content-Type': 'application/json'
+                        }
+                    })
+                    .then((response) => response.json())
+                    .then((data) => console.log(data))
+            },
+
+            //FETCH EVENTOS
+            verEventos: () => {
+                // EL try INTENTA HACER LO QUE ENCUENTRA ENTRE "{}". SI NO FUNCIONA, OMITE LA LOGICA QUE AHI SE ENCUENTRA
+                fetch(process.env.BACKEND_URL + "/api/leerEventos"), {
+                        method: "GET",
+                        body: JSON.stringify({
+
+                        }),
+                        headers: {
+                            'Content-Type': 'application/json'
+                        }
+                    }
+                    .then((response) => response.json())
+                    .then((data) => console.log(data.results))
+            },
+
+            changeColor: (index, color) => {
+                //get the store
+                const store = getStore();
+
+                //we have to loop the entire demo array to look for the respective index
+                //and change its color
+                const demo = store.demo.map((elm, i) => {
+                    if (i === index) elm.background = color;
+                    return elm;
+                });
+
+                //reset the global store
+                setStore({
+                    demo: demo,
+                });
+            },
+        },
+    };
 };
 
 export default getState;
