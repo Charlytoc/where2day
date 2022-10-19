8ad4ea82da1a518f09ec11d7086a66149ab55979
Wishmastering <charsdb@gmail.com>
2022-10-19 18:09:11 +0000

    Load Experiencias funcional, aun NO estetico al 100% - ARREGLADO

--- src/front/js/component/mostrarExp.js
+++ src/front/js/component/mostrarExp.js
@@ -10,27 +10,18 @@ export const MostrarExp = (props) => {
 
   const { store, actions } = useContext(Context); // #3 Consumirlo
 
-<<<<<<< HEAD
     // ACA LLAMAMOS LA FUNCION QUE HACE EL FETCH QUE VA TRAER LAS EXPERIENCIAS QUE TENEMOS DB
 
 
 
-=======
-  
->>>>>>> 1caf4f0a330e699a4d7358c9daafdaea7e2394fe
   return (
    <>
      <div className="card">
               {/* CARD FEED EXPERIENCIAS */}
               <h5 className="card-header bg-yellw2d">{props.fecha}</h5>
               <div className="card-body">
-<<<<<<< HEAD
                 <h4 className="card-title">{props.title} </h4>
                 <p className="card-text">
-=======
-                <h4 className="card-title"> Visita a la isla Santay </h4>
-                <div className="card-text">
->>>>>>> 1caf4f0a330e699a4d7358c9daafdaea7e2394fe
                   <img
                     src="https://previews.123rf.com/images/danflcreativo/danflcreativo1706/danflcreativo170600254/80138855-guayaquil-ecuador-mayo-2016-r%C3%ADo-guayas-en-la-vista-de-la-isla-santay-desde-el-malec%C3%B3n-2000-guayaquil.jpg"
                     className="d-block w-100"
@@ -40,17 +31,8 @@ export const MostrarExp = (props) => {
                     <b>Lugar:</b> {props.ubicacion}
                     <b> Lugar por:</b> {props.publicacion}
                   </p>
-<<<<<<< HEAD
                  
                 </p>
-=======
-                  Lorem ipsum dolor sit amet consectetur adipisicing
-                  elit.Quibusdam eum nostrum fuga, minus veritatis sed
-                  dignissimos, cupiditate asperiores ipsum quaerat inventore
-                  eaque, nesciunt nobis laudantium laborum itaque repellendus
-                  ipsa quia2..
-                </div>
->>>>>>> 1caf4f0a330e699a4d7358c9daafdaea7e2394fe
               </div>
             </div>
    </>
