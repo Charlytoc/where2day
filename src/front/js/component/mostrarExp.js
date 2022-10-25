558f217f2ab0e4f2e821dfe28b22e38f7852e987
Wishmastering <charsdb@gmail.com>
2022-10-25 02:07:03 +0000

    Display de LAPIZ DE EDIT si el props.ownerExp y el usuar_Actual de flux son el mismo ID

--- src/front/js/component/mostrarExp.js
+++ src/front/js/component/mostrarExp.js
@@ -15,12 +15,20 @@ export const MostrarExp = (props) => {
 
 
     return (
-        <><div className="mt-5 container bg-light rounded pub-title" >
+        <><div className="card mt-5 container bg-light rounded pub-title" >
+            <div className="card-header d-flex justify-content-between">
             <h3 className="text-dark mt-2 p-3 text-center pub-title">{props.title}</h3>
+            {store.usuario_actual === props.expOwner && <button className="btn"> 🖊 </button> }
+            </div>
             <div className="text-center"><img src={logo} className="w-75"></img></div>
             <div className=" container text-center">
-            <h4 className="text-dark text-center d-inline-block" title="Vivida por">🧔🏻 {props.expOwner}</h4>
-            <h4 className="text-dark text-center d-inline-block" title="Lugar">🔍 {props.lugar}</h4>
+            {/* <h4 className="text-dark text-center d-inline-block" title="Vivida por">🧔🏻 {props.expOwner}</h4> */}
+            
+                <h4 className="text-dark text-center d-inline-block" title="Lugar">🔍 {props.lugar}</h4>
+               
+            
+            
+            
             <h4 className="text-dark text-center d-inline-block" title="Fecha">🗓️ {props.fecha}</h4>
             { props.outdoor ? <h4 className="text-dark text-center d-inline-block" title="Outdoor">🏃🏻</h4> : null}
             { props.indoor ? <h4 className="text-dark text-center d-inline-block" title="Indoor">🏠</h4> : null}
