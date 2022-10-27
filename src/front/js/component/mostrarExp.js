00386d6b60f1d89b1da7daed7a2eab72ede8d805
Wishmastering <charsdb@gmail.com>
2022-10-27 00:16:28 +0000

    onClick DISPARA BOOLEANO de MOSTRA/NO MOSTAR edit, ya recibiendo para titulo, pendiente la funcion para enviar el post a endpoint

--- src/front/js/component/mostrarExp.js
+++ src/front/js/component/mostrarExp.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import { Link } from "react-router-dom";
 import logo from "../../img/logo-where2-alone.png";
 import logoPres from "../../img/Logo WHERE2DAY.png";
@@ -12,26 +12,64 @@ export const MostrarExp = (props) => {
 
     // ACA LLAMAMOS LA FUNCION QUE HACE EL FETCH QUE VA TRAER LAS EXPERIENCIAS QUE TENEMOS DB
 
-
+    const [titulo, setTitulo] = useState(props.title)
+    
+    const [desplegar, setDesplegar] = useState(false)
+    const desplegarEdit = () => {
+        desplegar ? setDesplegar(false) : setDesplegar(true)
+        // console.log(desplegar)
+    }
 
     return (
-        <><div className=" card " >
-            <div className="card-header d-flex justify-content-between">
-                <h3 >{props.title}</h3>
-                {props.expOwner === store.usuario_actual && <button className="btn"> ✏</button> }
+        <>
+            <div className=" card " >
+            {/* Empieza el booleano desplegarEdit, depende si true o false */}
+            {/* Mostraremos entoncs el INPUT para editar o el Post normal */}
+            {desplegar ? 
+            <>
+                <div className="card-header d-flex justify-content-between">
+                <input onChange={(e) => {setTitulo(e.target.value)}} value={titulo}
+                type="text" className="mt-1 form-control" />
+                    {props.expOwner === store.usuario_actual 
+                    && 
+                    <button className="btn" onClick={desplegarEdit}> 🖊</button> }
+                </div>
+                <div className="text-center"><img src={logo} className="w-75"></img></div>
+                <div className=" container text-center">   
+                    <h4 className="text-dark text-center d-inline-block" title="Lugar">🔍 {props.lugar}</h4>
+                    <h4 className="text-dark text-center d-inline-block" title="Fecha">🗓️ {props.fecha}</h4>
+                    { props.outdoor ? <h4 className="text-dark text-center d-inline-block" title="Outdoor">🏃🏻</h4> : null}
+                    { props.indoor ? <h4 className="text-dark text-center d-inline-block" title="Indoor">🏠</h4> : null}
+                    { props.anywhere ? <h4 className="text-dark text-center d-inline-block" title="Anywhere">🌐</h4> : null}
+                </div>
+                
+                <h4 className="text-dark text-center fs-6 p-5">✍🏻{props.description}</h4>
+                <button  className=" btn-outline border-0 rounded btn-lg navarra animable2 mb-3 boton3 mt-0"> Edit Post</button>
+            </>
+        :
+             <>
+                <div className="card-header d-flex justify-content-between">
+                    <h3 >{props.title}</h3>
+                    {props.expOwner === store.usuario_actual 
+                    && 
+                    <button className="btn" onClick={desplegarEdit}> 🖊</button> }
+                </div>
+                <div className="text-center"><img src={logo} className="w-75"></img></div>
+                <div className=" container text-center">   
+                    <h4 className="text-dark text-center d-inline-block" title="Lugar">🔍 {props.lugar}</h4>
+                    <h4 className="text-dark text-center d-inline-block" title="Fecha">🗓️ {props.fecha}</h4>
+                    { props.outdoor ? <h4 className="text-dark text-center d-inline-block" title="Outdoor">🏃🏻</h4> : null}
+                    { props.indoor ? <h4 className="text-dark text-center d-inline-block" title="Indoor">🏠</h4> : null}
+                    { props.anywhere ? <h4 className="text-dark text-center d-inline-block" title="Anywhere">🌐</h4> : null}
+                </div>
+                
+                <h4 className="text-dark text-center fs-6 p-5">✍🏻{props.description}</h4>
+            </>
+                
+            }
+
+                
             </div>
-            <div className="text-center"><img src={logo} className="w-75"></img></div>
-            <div className=" container text-center">
-            
-            <h4 className="text-dark text-center d-inline-block" title="Lugar">🔍 {props.lugar}</h4>
-            <h4 className="text-dark text-center d-inline-block" title="Fecha">🗓️ {props.fecha}</h4>
-            { props.outdoor ? <h4 className="text-dark text-center d-inline-block" title="Outdoor">🏃🏻</h4> : null}
-            { props.indoor ? <h4 className="text-dark text-center d-inline-block" title="Indoor">🏠</h4> : null}
-            { props.anywhere ? <h4 className="text-dark text-center d-inline-block" title="Anywhere">🌐</h4> : null}
-            </div>
-            
-            <h4 className="text-dark text-center fs-6 p-5">✍🏻{props.description}</h4>
-        </div>
         
         </>
 
