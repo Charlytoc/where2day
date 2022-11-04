4277778eb31aba98517aac8f1aad6cbfed0ca55e
Franz Seidel <franz8818@gmail.com>
2022-11-04 04:56:59 +0000

    definitivo

--- src/front/js/component/headerProfile.js
+++ src/front/js/component/headerProfile.js
@@ -72,46 +72,46 @@ export const HeaderProfile = () => {
         <>
             <div className="container border w-75 shadow">
 
-                {desplegar ?<>
+                {desplegar ? <>
 
                     <div className="row">
                         <div className="col-3">
-                        <img
+                            <img
                                 className="profile-pic w-100 rounded-circle justify-content-center"
                                 src={store.profile.image_url}
                                 alt="profile"
                             />
-                        
+
                         </div>
                         <div className="col-9 container">
                             <div className="row mt-3">
-                            <div className="col-6"><input onChange={(e) => { setUsername(e.target.value) }} value={username} placeholder="Username"
+                                <div className="col-6"><input onChange={(e) => { setUsername(e.target.value) }} value={username} placeholder="Username"
                                     type="text" className=" form-control justify-content-center border-0" /></div>
-                            <div className="col-6"><button onClick={editProfile} className="btn border click" > Editar perfil</button></div>
+                                <div className="col-6"><button onClick={editProfile} className="btn border click" > Editar perfil</button></div>
                             </div>
                             <div className="row mt-3">
                                 <div className="col-1"></div>
                                 <div className="col-5"> <input onChange={(e) => { setNombre(e.target.value) }} value={nombre} placeholder="Ingresa Tu Nombre"
-                                type="text" className="mt-1 form-control border-0 justify-content-center text-center" /></div>
-                                
+                                    type="text" className="mt-1 form-control border-0 justify-content-center text-center" /></div>
+
                                 <div className="col-5"><input onChange={(e) => { setPais(e.target.value) }} value={pais} placeholder="País"
                                     type="text" className="mt-1 form-control border-0 justify-content-center text-center mb-2" /></div>
                                 <div className="col-1"></div>
                             </div>
                             <div className="row">
-                            <div className="col-1"></div>
-                            <div className="col-5"> <input onChange={(e) => { setApellido(e.target.value) }} value={apellido} placeholder="Ingresa Tu Apellido"
-                                type="text" className="mt-1 form-control border-0 justify-content-center text-center" /></div>
-                            <div className="col-5"> <input onChange={(e) => { setEdad(e.target.value) }} value={edad} placeholder="Ingresa tu edad"
-                                type="text" className="mt-1 form-control border-0 justify-content-center text-center" /></div>
-                            <div className="col-1"></div>
+                                <div className="col-1"></div>
+                                <div className="col-5"> <input onChange={(e) => { setApellido(e.target.value) }} value={apellido} placeholder="Ingresa Tu Apellido"
+                                    type="text" className="mt-1 form-control border-0 justify-content-center text-center" /></div>
+                                <div className="col-5"> <input onChange={(e) => { setEdad(e.target.value) }} value={edad} placeholder="Ingresa tu edad"
+                                    type="text" className="mt-1 form-control border-0 justify-content-center text-center" /></div>
+                                <div className="col-1"></div>
                             </div>
                             <div className="row">
                                 <div className="col-1"></div>
                                 <div className="col-5"> <input onChange={(e) => { setEmail(e.target.value) }} value={email} placeholder="Ingresa tu correo"
-                                type="text" className="mt-1 border-0 form-control justify-content-center text-center" /></div>
+                                    type="text" className="mt-1 border-0 form-control justify-content-center text-center" /></div>
                                 <div className="col-5"> <input onChange={(e) => { setPassword(e.target.value) }} value={password} placeholder="Introduce Tu Contraseña Aqui"
-                                type="password" className="mt-1 form-control justify-content-center text-center" /></div>
+                                    type="password" className="mt-1 form-control justify-content-center text-center" /></div>
                                 <div className="col-1"></div>
                             </div>
                             <div className="row">
@@ -119,53 +119,53 @@ export const HeaderProfile = () => {
                                 <div className="col-5"><input onChange={(e) => { uploadImage(e) }}
                                     type="file" className="mt-1 form-control justify-content-center text-center" /></div>
                                 <div className="col-5"><button className="btn border mt-2" onClick={submitChanges}>
-                                <FontAwesomeIcon icon={faPen} />
-                                Guardar cambios
-                            </button></div>
+                                    <FontAwesomeIcon icon={faPen} />
+                                    Guardar cambios
+                                </button></div>
                                 <div className="col-1"></div>
                             </div>
 
                         </div>
                     </div>
 
-                    </>
+                </>
                     :
-                        <>
-                    
-                    <div className="row">
-                        <div className="col-3">
-                        <img
-                                className="profile-pic w-100 rounded-circle justify-content-center"
-                                src={store.profile.image_url}
-                                alt="profile"
-                            />
-                        </div>
-                        <div className="col-9 container">
-                            <div className="row mt-3">
-                            <div className="col-6">{store.profile.username === null ? <h2><b>Elije Un Username</b></h2> : <h2 className="profile-name text-black">{store.profile.username}</h2>}</div>
-                            <div className="col-6"><button onClick={editProfile} className="btn border click" > Editar perfil</button></div>
-                            </div>
-                            <div className="row mt-3">
-                                <div className="col-1"></div>
-                                <div className="col-5">{store.profile.nombre === null ? <p><b>Confirma Tu Nombre</b></p> : <p className="fs-5"> {store.profile.nombre}</p>}</div>
-                                
-                                <div className="col-5">{store.profile.pais === null ? <p><b>Elije Tu Ubicacion</b></p> : <p className="fs-5 text-black">{store.profile.pais}</p>}</div>
-                                <div className="col-1"></div>
-                            </div>
-                            <div className="row">
-                            <div className="col-1"></div>
-                            <div className="col-5"> {store.profile.apellido === null ? <p><b>Confirma Tu Apellido</b></p> : <p className="fs-5"> {store.profile.apellido}</p>}</div>
-                            <div className="col-5"> {store.profile.edad === null ? <p><b>Confirma Tu Edad</b></p> : <p className="fs-5"> {store.profile.edad}</p>}</div>
-                            <div className="col-1"></div>
-                            </div>
-                            <div className="row">
-                                <div className="col-1"></div>
-                                <div className="col-5">{store.profile.correo === null ? <p><b>Confirma Tu eMail</b></p> : <h4> {store.profile.email}</h4>}</div>
-                                <div className="col-6"></div>
-                            </div>
+                    <>
 
+                        <div className="row">
+                            <div className="col-3">
+                                <img
+                                    className="profile-pic w-100 rounded-circle justify-content-center"
+                                    src={store.profile.image_url}
+                                    alt="profile"
+                                />
+                            </div>
+                            <div className="col-9 container">
+                                <div className="row mt-3">
+                                    <div className="col-6">{store.profile.username === null ? <h2><b>Elije Un Username</b></h2> : <h2 className="profile-name text-black">{store.profile.username}</h2>}</div>
+                                    <div className="col-6"><button onClick={editProfile} className="btn border click" > Editar perfil</button></div>
+                                </div>
+                                <div className="row mt-3">
+                                    <div className="col-1"></div>
+                                    <div className="col-5">{store.profile.nombre === null ? <p><b>Confirma Tu Nombre</b></p> : <p className="fs-5"> {store.profile.nombre}</p>}</div>
+
+                                    <div className="col-5">{store.profile.pais === null ? <p><b>Elije Tu Ubicacion</b></p> : <p className="fs-5 text-black">{store.profile.pais}</p>}</div>
+                                    <div className="col-1"></div>
+                                </div>
+                                <div className="row">
+                                    <div className="col-1"></div>
+                                    <div className="col-5"> {store.profile.apellido === null ? <p><b>Confirma Tu Apellido</b></p> : <p className="fs-5"> {store.profile.apellido}</p>}</div>
+                                    <div className="col-5"> {store.profile.edad === null ? <p><b>Confirma Tu Edad</b></p> : <p className="fs-5"> {store.profile.edad}</p>}</div>
+                                    <div className="col-1"></div>
+                                </div>
+                                <div className="row">
+                                    <div className="col-1"></div>
+                                    <div className="col-5">{store.profile.correo === null ? <p><b>Confirma Tu eMail</b></p> : <h4> {store.profile.email}</h4>}</div>
+                                    <div className="col-6"></div>
+                                </div>
+
+                            </div>
                         </div>
-                    </div>
                     </>
                 }
 
--- src/front/js/component/infoProfile.js
+++ src/front/js/component/infoProfile.js
@@ -6,7 +6,7 @@ import logoPres from "../../img/Logo WHERE2DAY.png";
 import { useContext } from "react"; // #1 Traer context de react
 import { Context } from "../store/appContext"; // #2 traer nuestro context
 
-export const InfoProfile = ({setStore}) => {
+export const InfoProfile = ({ setStore }) => {
     const { store, actions } = useContext(Context); // #3 Consumirlo
 
     const actualizarPost = (titulo, lugar, image_url, descripcion, fecha) => {
@@ -24,20 +24,17 @@ export const InfoProfile = ({setStore}) => {
     console.log(store.feedExperiencias)
     return (
         <>
-            <div className="card text-bg-light mb-3">
+            <div className="card shadow bg-body fijar-fil rounded my-3 m-0 float-start">
                 {/* CARD EVENTOS */}
                 <h6>
-                    <div className="card-header font-weight-bold bg-yellw2d">
-                        Mis eventos/otros
+                    <div className="btn-outline mb-0 rounded p-3 text-center selected">
+                        Mis experiencias
                     </div>
                 </h6>
-                <ul className="list-group list-group-flush">
-                    {store.feedExperiencias.map((item, index) => 
-                    <li key={index} onClick={()=>{actualizarPost(item.titulo, item.lugar, item.image_url, item.descripcion, item.fecha)}} className="list-group-item hand" >{item.titulo}</li>)}
-                
-                    
+                <ul className="list-group mt-0 list-group-flush">
+                    {store.feedExperiencias.map((item, index) =>
+                        <li key={index} onClick={() => { actualizarPost(item.titulo, item.lugar, item.image_url, item.descripcion, item.fecha) }} className="list-group-item click" >{item.titulo}</li>)}
                 </ul>
-                <div className="card-footer bg-yellw2d">Actividades</div>
             </div>
         </>
     );
